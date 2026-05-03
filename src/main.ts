d459dae86692d981cd695faa0a2aa9780c121cdd
Emma Pang <emmapang3@gmail.com>
2026-05-03 15:22:35 +0200

    improved validation and added loading text

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,32 @@ import { showError } from "./utils/createErrorMsg";
 import { getArtworks } from "./services/artworkService";
 import { createArtworkCard } from "./utils/createArtworkCard";
 
+const input = document.getElementById("searchArtwork") as HTMLInputElement;
+const artSection = document.getElementById("artSection");
+
 document.getElementById("form")?.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const input = document.getElementById("searchArtwork");
-
-  let searchArtwork = "";
-
-  if (input) {
-    searchArtwork = (input as HTMLInputElement).value.trim();
+  if (!input.value) {
+    showError("Please enter a search term");
+    return;
   }
 
-  const artworks = await getArtworks(searchArtwork);
+  try {
+    if (artSection) {
+      artSection.innerHTML = "";
+      artSection.innerHTML =
+        '<p class="text-center text-black col-span-12">Loading...</p>';
+    }
 
-  if (!artworks) {
-    return showError("Artworks not found");
-  }
+    const artworks = await getArtworks(input.value.trim());
 
-  createArtworkCard(artworks);
+    createArtworkCard(artworks);
 
-  if (input) {
-    (input as HTMLInputElement).value = "";
+    input.value = "";
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Failed to fetch artworks";
+    showError(message);
   }
 });
